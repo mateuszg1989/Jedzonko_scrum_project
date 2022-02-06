75ea60c32cb6c38f30e35b51756b621199c0cc06
Tomasz Bladyniec <tomasz.bladyniec@protonmail.com>
2022-02-07 00:13:00 +0100

    zadanie 1.2

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# test
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=128)
+    ingredients = models.TextField()
+    description = models.TextField()
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    preparation_time = models.IntegerField(null=True)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
