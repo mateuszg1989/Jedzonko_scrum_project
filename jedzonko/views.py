5535ee8550b5dcabd4d97b06d17ebbe2f20e415c
Arkadiusz <arkadiuszlaskowski1987@gmail.com>
2022-02-07 14:07:34 +0100

    formularz do recipe zad 4.1 i 4.2

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from jedzonko.models import Recipe
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from random import shuffle
+from jedzonko.forms import RecipeForm
+
 
 class IndexView(View):
 
@@ -22,6 +24,21 @@ class LandingPageView(View):
 class DashboardView(View):
 
     def get(self, request):
-
         return render(request, "dashboard.html")
 
+class AddRecipeView(View):
+    def get(self,request):
+        form = RecipeForm()
+        return render(request,'app-add-recipe.html',{'form':form})
+
+    def post(self,request):
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            description = form.cleaned_data['description']
+            preparation_time = form.cleaned_data['preparation_time']
+            ingredients = form.cleaned_data['ingredients']
+            Recipe.objects.create(name=name, description=description, preparation_time=preparation_time, ingredients=ingredients)
+            return redirect('/recipe/list')
+        else:
+            return render(request,'app-add-recipe.html',{'form':form})
