32d67048acb302e9e5bb55a3ad9f067c6959ee58
Tomasz Bladyniec <tomasz.bladyniec@protonmail.com>
2022-02-07 14:18:56 +0100

    reszta modeli

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -13,4 +13,40 @@ class Recipe(models.Model):
     def __str__(self):
         return self.name
 
-# test3
+
+class Plan(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    created = models.DateField(auto_now_add=True)
+    recipes = models.ManyToManyField(Recipe, through='RecipePlan')
+
+    def __str__(self):
+        return self.name
+
+
+class DayName(models.Model):
+    name = models.CharField(max_length=16)
+    order = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class RecipePlan(models.Model):
+    meal_name = models.CharField(max_length=16)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    order = models.IntegerField(unique=True)
+    day_name = models.ForeignKey(DayName, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.meal_name
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    slug = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.title
